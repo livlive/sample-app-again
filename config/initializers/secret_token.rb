d3a80b2b6842ff7aa5a8d282958bcc2fb120a639
Scott MoorHouse <scottmoorhouse@gmail.com>
2012-05-24 13:33:13 -0500

    New App Baby

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '9d7607b207b5a1685e337fb4486954ddb09002ee7f573ac63d0b41de3973a9d158ea07159365683357836b98865ff306e830c27bf6ac932610af2f93f4719d8e'
